014651e3bb694a1dea216354b67d51dcdf034ac7
Michael Werbowsky <michaelwerbowsky@Michaels-MacBook-Pro.local>
2026-03-26 23:53:11 -0600

    move root redirect to next

--- field-app/next.config.ts
+++ field-app/next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/athlete',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
